678b3510d198316d5189acef4138911e6e2114bf
JavadAbdi <Mirjavadabdi@gmail.com>
2024-03-25 04:36:23 +0330

    Chanegd,Updated

--- src/main/java/com/javatar/spring/security/login/models/Role.java
+++ src/main/java/com/javatar/spring/security/login/models/Role.java
@@ -1,10 +1,10 @@
 package com.javatar.spring.security.login.models;
 
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "roles")
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
