9934db46348f9d85b8ff96ec71783c567956f92c
JavadAbdi <Mirjavadabdi@gmail.com>
2024-03-28 15:48:28 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/Role.java
+++ src/main/java/com/javatar/spring/security/login/models/Role.java
@@ -1,6 +1,7 @@
 package com.javatar.spring.security.login.models;
 
 import jakarta.persistence.*;
+
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -12,7 +13,6 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private ERole name;
-
     public Role() {
 
     }
