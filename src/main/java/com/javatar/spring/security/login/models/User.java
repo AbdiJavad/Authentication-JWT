e192880651b9d8ddc447a74be0b414ee5e2bd66c
JavadAbdi <Mirjavadabdi@gmail.com>
2024-03-30 01:35:09 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/User.java
+++ src/main/java/com/javatar/spring/security/login/models/User.java
@@ -46,9 +46,11 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
