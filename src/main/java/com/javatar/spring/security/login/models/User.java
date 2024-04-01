1ed06f25403e4a151029f853bb3fabf4a0d19988
JavadAbdi <Mirjavadabdi@gmail.com>
2024-04-01 17:52:54 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/User.java
+++ src/main/java/com/javatar/spring/security/login/models/User.java
@@ -36,7 +36,6 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
-
     public User() {
     }
 
