2eafcd340e17a94583baee5681b5915c88fc49cd
JavadAbdi <Mirjavadabdi@gmail.com>
2024-04-02 06:36:22 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/Role.java
+++ src/main/java/com/javatar/spring/security/login/models/Role.java
@@ -9,7 +9,6 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private ERole name;
