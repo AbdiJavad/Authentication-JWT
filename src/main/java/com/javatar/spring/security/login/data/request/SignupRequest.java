e9174fb8a1ff762a8f600e0d005bb71431012a35
JavadAbdi <Mirjavadabdi@gmail.com>
2024-04-06 20:24:32 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/data/request/SignupRequest.java
+++ src/main/java/com/javatar/spring/security/login/data/request/SignupRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.util.Set;
- 
+
 public class SignupRequest {
     @NotBlank
     @Size(min = 3, max = 20)
@@ -18,36 +18,36 @@ public class SignupRequest {
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
-  
+
     public String getUsername() {
         return username;
     }
- 
+
     public void setUsername(String username) {
         this.username = username;
     }
- 
+
     public String getEmail() {
         return email;
     }
- 
+
     public void setEmail(String email) {
         this.email = email;
     }
- 
+
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public Set<String> getRole() {
-      return this.role;
+        return this.role;
     }
-    
+
     public void setRole(Set<String> role) {
-      this.role = role;
+        this.role = role;
     }
 }
