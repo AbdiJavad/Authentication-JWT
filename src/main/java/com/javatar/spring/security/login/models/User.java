2e72c71778e83226cf45525411c7b2b4035847b3
JavadAbdi <Mirjavadabdi@gmail.com>
2024-04-29 21:08:16 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/User.java
+++ src/main/java/com/javatar/spring/security/login/models/User.java
@@ -15,6 +15,8 @@ import java.util.Set;
                 @UniqueConstraint(columnNames = "email")
         })
 public class User {
+
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
