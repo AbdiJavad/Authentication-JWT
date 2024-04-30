0a2b860ef4203122d2720a0008e38edcc89e6ae7
JavadAbdi <Mirjavadabdi@gmail.com>
2024-04-30 16:59:05 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/User.java
+++ src/main/java/com/javatar/spring/security/login/models/User.java
@@ -14,6 +14,7 @@ import java.util.Set;
                 @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
         })
+
 public class User {
 
     @Id
