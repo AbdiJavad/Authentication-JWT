6960af47a6e1ae10e67c4840d3c138393d584e21
JavadAbdi <Mirjavadabdi@gmail.com>
2024-06-19 22:02:21 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/models/Role.java
+++ src/main/java/com/javatar/spring/security/login/models/Role.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "roles")
 public class Role {
 
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -17,6 +17,7 @@ public class Role {
     public Role() {
     }
 
+    
     public Role(ERole name) {
         this.name = name;
     }
