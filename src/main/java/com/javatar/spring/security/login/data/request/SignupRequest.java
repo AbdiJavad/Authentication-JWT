dfa24232e44ff075953eb9e39f58f77c047bbb55
JavadAbdi <Mirjavadabdi@gmail.com>
2024-10-25 20:35:00 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/data/request/SignupRequest.java
+++ src/main/java/com/javatar/spring/security/login/data/request/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.javatar.spring.security.login.data.request;
 
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
