8852f5831a508a07cbf3154e7a47c6b07ae90483
JavadAbdi <Mirjavadabdi@gmail.com>
2024-10-27 16:11:44 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/controllers/AuthController.java
+++ src/main/java/com/javatar/spring/security/login/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.javatar.spring.security.login.controllers;
 
-
 import com.javatar.spring.security.login.models.ERole;
 import com.javatar.spring.security.login.models.Role;
 import com.javatar.spring.security.login.models.User;
