f21de912769763802664d9bddae9c4f6e41e860b
JavadAbdi <Mirjavadabdi@gmail.com>
2024-11-06 00:07:56 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/controllers/TestController.java
+++ src/main/java/com/javatar/spring/security/login/controllers/TestController.java
@@ -1,6 +1,5 @@
 package com.javatar.spring.security.login.controllers;
 
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
