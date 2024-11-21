1b5d8654f085a6dd0d4928dedf799df8ce42cf47
JavadAbdi <Mirjavadabdi@gmail.com>
2024-11-21 17:14:42 +0330

    Updated,Changed

--- src/main/java/com/javatar/spring/security/login/controllers/AuthController.java
+++ src/main/java/com/javatar/spring/security/login/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.javatar.spring.security.login.controllers;
 
-
 import com.javatar.spring.security.login.models.ERole;
 import com.javatar.spring.security.login.models.Role;
 import com.javatar.spring.security.login.models.User;
@@ -31,7 +30,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    
+
     @Autowired
     AuthenticationManager authenticationManager;
     @Autowired
